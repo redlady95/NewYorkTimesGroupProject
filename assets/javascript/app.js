fa37dcc8cf7f8839579782be526c3ba3bfadf94e
redlady95 <laurasedok76@gmail.com>
2019-02-28 21:05:12 -0600

    hello

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+ 
+        //on click event
+        $("#search").on("click", function () {
+
+            //create a query
+            //var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats";
+
+
+            var iniDate = 20120101;
+            var endDate = 20190228;
+            var query = $("#exampleFormControlInput1").val().trim(); 
+            var filter = 'news_desk:("Sports") AND glocations:("NEW YORK CITY")';
+            var apiKey = "xD3kkxS8Ju3BRR99VVaBCrAdJVFinyEg";
+
+            //   var queryURL = `/articlesearch.json?q=${query}&fq=${filter}`;
+            //   var queryURL = "/articlesearch.json?q="+query+"&fq="+filter;
+            //var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=xD3kkxS8Ju3BRR99VVaBCrAdJVFinyEg"
+            var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&fq=" + filter + "begin_date=" + iniDate + + "end_date=" + endDate + "&api-key=" + apiKey;
+
+            //ajax call using the query  
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+
+                //promise function
+                .then(function (response) {
+
+                    console.log(response);
+
+                });
+        });
+  
